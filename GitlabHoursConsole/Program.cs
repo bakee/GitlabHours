1e30a08d3652d69261275f3e7f52fc5aaa73fc7d
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2021-01-15 15:21:40 +0600

    Add test console app

---
+++ GitlabHoursConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GitlabHoursLib;
+
+namespace GitlabHoursConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(userInput) && userInput.ToLower() == "exit")
+                {
+                    return;
+                }
+
+                var output = new HourConverter().Convert(userInput);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
